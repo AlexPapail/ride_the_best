b2c80942c36bff3327107cba2184f574167eead0
Alex <alexandre.papail@josette-tic.com>
2025-11-26 15:27:58 +0100

    tentative d'installation de taiwind 2

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/app.js')
+    .addStyleEntry('styles', './assets/styles/app.css')
+    .enablePostCssLoader()
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+module.exports = Encore.getWebpackConfig();
+
+
