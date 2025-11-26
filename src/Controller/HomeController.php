7b1e5debf44c5fdd09ac00ccbc2cdf245e28da53
Alex <alexandre.papail@josette-tic.com>
2025-11-26 11:57:25 +0100

    tentative d'installation de taiwind

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,14 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class TestController extends AbstractController
+final class HomeController extends AbstractController
 {
-    #[Route('/test', name: 'app_test')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
-        ]);
+        return $this->render('/index.html.twig');
     }
 
 }
